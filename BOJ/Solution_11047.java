7adfee457a5cc6a593d7d0177a821a51606971cb
YSH <yang5864@naver.com>
2026-03-16 13:39:08 +0900

    260316 실버4 그리디

---
+++ BOJ/Solution_11047.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+
+        for (int i = N - 1; i >= 0 ; i--) {
+            cnt += K / coin[i];
+            K %= coin[i];
+        }
+        System.out.println(cnt);
+    }
+}
