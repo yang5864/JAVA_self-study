de2a166a63cb95e575018969cf84fca3b6631232
YSH <yang5864@naver.com>
2026-03-25 19:18:28 +0900

    260325 Lv.2 그리디(설명 추가)

--- BOJ/Solution_GymClothes.java
+++ BOJ/Solution_GymClothes.java
@@ -9,7 +9,7 @@ public class Solution_GymClothes {
 
         for (int i = 0; i < lostSize; i++) {
             for (int j = 0; j < reserveSize; j++) {
-                if (reserve[j] == lost[i]) {
+                if (reserve[j] == lost[i]) {    // 여벌 있는 학생이 도난당한 경우
                     reserve[j] = -1;
                     lost[i] = -1;
                     answer++;
@@ -22,11 +22,11 @@ public class Solution_GymClothes {
         Arrays.sort(reserve);
 
         for (int i = 0; i < lostSize; i++) {
-            if (lost[i] == -1) continue;
+            if (lost[i] == -1) continue;    // 여벌 있는데 도난당한 경우 스킵
             for (int j = 0; j < reserveSize; j++) {
                 if (((lost[i] + 1) == reserve[j]) || ((lost[i] - 1) == reserve[j])) {
                     answer += 1;
-                    reserve[j] = -1;
+                    reserve[j] = -1;    //이미 나눠줬으니까 없음 이제
                     break;
                 }
             }
